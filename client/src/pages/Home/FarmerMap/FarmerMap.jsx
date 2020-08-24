04efb37aca53bb7f1e4d9c4c26d014ab298eee5a
guido <guidodutra@gmail.com>
2020-08-24 16:05:46 -0300

    fix map marker farmer sync

--- client/src/pages/Home/FarmerMap/FarmerMap.jsx
+++ client/src/pages/Home/FarmerMap/FarmerMap.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {
   GoogleApiWrapper,
@@ -9,74 +9,79 @@ import {
 import { defaultCoordinates, googleApiKey } from '../../../config';
 import styles from './FarmerMap.module.css';
 
-const FarmerMap = ({
-  google,
-  style,
-  farmers,
-  selectedFarmer,
-  selectFarmerCallback,
-}) => {
-  const [isInfoWindowOpen, setIsInfoWindowOpen] = useState(false);
-  const [activeMarker, setActiveMarker] = useState({});
+class FarmerMap extends React.Component {
+  constructor(props) {
+    super(props);
+    props.farmers.forEach(farmer => {
+      this[`marker${farmer.id}`] = React.createRef();
+    });
+  }
 
-  const onMarkerClick = (farmer, marker) => {
-    selectFarmerCallback(farmer);
-    setActiveMarker(marker);
-    setIsInfoWindowOpen(true);
-  };
+  render() {
+    const {
+      google,
+      style,
+      farmers,
+      selectedFarmer,
+      selectFarmerCallback,
+    } = this.props;
 
-  const onMapClicked = () => {
-    if (isInfoWindowOpen) {
-      setIsInfoWindowOpen(false);
-      // setActiveMarker({});
-      // setSelectedPlace({});
-    }
-  };
+    const activeMarker = selectedFarmer ? this[`marker${selectedFarmer.id}`].current?.marker : null;
+    const isInfoWindowOpen = Boolean(selectedFarmer);
 
-  return (
-    <div
-      style={{
-        ...style,
-      }}
-    >
-      <Map
-        onClick={onMapClicked}
-        google={google}
-        zoom={11}
-        initialCenter={defaultCoordinates}
-        containerStyle={{
-          position: 'relative',
-          minWidth: '300px',
-          minHeight: '300px',
+    const onMarkerClick = (farmer) => {
+      selectFarmerCallback(farmer);
+    };
+
+    const onMapClicked = () => {
+      selectFarmerCallback(null);
+    };
+
+    return (
+      <div
+        style={{
+          ...style,
         }}
       >
-        {farmers.map(farmer => farmer.latitude && (
-          <Marker
-            // eslint-disable-next-line no-shadow
-            onClick={({ farmer }, marker) => onMarkerClick(farmer, marker)}
-            key={farmer.id}
-            // ref={farmer.id}
-            farmer={farmer}
-            title={farmer.name}
-            position={{
-              lat: farmer.latitude,
-              lng: farmer.longitude,
-            }}
-          />
-        ))}
-        <InfoWindow
-          marker={activeMarker}
-          visible={isInfoWindowOpen}
+        <Map
+          onClick={onMapClicked}
+          google={google}
+          zoom={11}
+          initialCenter={defaultCoordinates}
+          containerStyle={{
+            position: 'relative',
+            minWidth: '300px',
+            minHeight: '300px',
+          }}
         >
-          <div className={styles.infoWindow}>
-            <h4>{selectedFarmer?.name}</h4>
-            {selectedFarmer?.description}
-          </div>
-        </InfoWindow>
-      </Map>
-    </div>
-  );
-};
+          {farmers.map(farmer => farmer.latitude && (
+            <Marker
+              // eslint-disable-next-line no-shadow
+              onClick={({ farmer }, marker) => onMarkerClick(farmer, marker)}
+              key={farmer.id}
+              ref={this[`marker${farmer.id}`]}
+              farmer={farmer}
+              title={farmer.name}
+              position={{
+                lat: farmer.latitude,
+                lng: farmer.longitude,
+              }}
+            />
+          ))}
+          <InfoWindow
+            marker={activeMarker}
+            visible={isInfoWindowOpen}
+          >
+            <div className={styles.infoWindow}>
+              <h4>{selectedFarmer?.name}</h4>
+              {selectedFarmer?.description}
+            </div>
+          </InfoWindow>
+        </Map>
+      </div>
+    );
+  }
+}
 
 FarmerMap.propTypes = {
   google: PropTypes.object.isRequired,
